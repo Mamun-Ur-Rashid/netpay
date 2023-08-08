cd1c9e17f6d593f7836cca25ff4d24c7783a3deb
rhrubelr <rhrubel246@gmail.com>
2023-08-08 17:01:18 +0600

    added banner

--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import Banner from "../../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h1 className='text-center text-5xl font-bold'>This is Home </h1>
             <h1 className='text-center text-5xl font-bold'>this is banner section</h1>
         </div>
