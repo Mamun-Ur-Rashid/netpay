abd2fbc3842210c018660ff25d5bc2373cb75d0e
nahian255 <nahimn253@gmail.com>
2023-08-10 17:42:28 +0600

    banner holder change

--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -1,9 +1,11 @@
-import Banner from "../../Banner/Banner";
+// import Banner from "../../Banner/Banner";
+import Banner from '../../Home/banner/Banner'
 import FinancialCard from "../../FinancialPlatform/FinancialCard/FinancialCard";
 
 const Home = () => {
     return (
         <div className="mt-16">
+            {/* <Banner></Banner> */}
             <Banner></Banner>
             <FinancialCard></FinancialCard>
 
