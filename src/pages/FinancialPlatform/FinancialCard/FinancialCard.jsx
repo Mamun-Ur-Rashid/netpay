b27f262f2495decc0ab1f789c9e0e58e89524569
muhammadali246397 <muhammadali246397@gmail.com>
2023-08-11 16:24:21 +0600

    complete card design

--- src/pages/FinancialPlatform/FinancialCard/FinancialCard.jsx
+++ src/pages/FinancialPlatform/FinancialCard/FinancialCard.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import cashin from '../../../assets/cashin.jpg'
+import payment from '../../../assets/payment.webp'
+import sendmoney from '../../../assets/sendmoney.webp'
+import cashout from '../../../assets/cashout.jpg'
 
 const FinancialCard = () => {
     return (
@@ -10,32 +13,34 @@ const FinancialCard = () => {
             <div className="my-10 grid grid-cols-1 md:grid-3 lg:grid-cols-4 gap-10 container mx-auto">
 
                 <Link to='/mobileRecharge'>
-                    <div className="card w-50 h-full bg-[#FFEAEA] shadow-2xl cursor-pointer">
+                    <div style={{boxShadow:'2px 2px 5px'}} className="card w-50 h-full bg-[#FFEAEA] shadow-[2px 0px 0px 0px] cursor-pointer">
                         <figure className="px-0 pt-0">
-                           <img className="w-full rounded-md" src={cashin} alt="" />
+                           <img className="w-full h-[264px] rounded-md" src={cashin} alt="" />
                         </figure>
                         <div className="card-body items-center text-center">
                             <h2 className="card-title text-2xl text-orange-700">Cash In</h2>
-                            <p>Recharge any number and get the best offer</p>
+                            <p>Depositing money into Customer Netpay Account</p>
                             <div className="card-actions">
                                 {/* <button className="btn btn-primary">Learn More</button>
                                  */}
-                                <Link className="text-red-700 font-semibold text-xl" to='/mobileRecharge'>Learn More</Link>
+                                <Link><button className="btn bg-orange-700 text-white px-6 py-2 hover:bg-orange-600 hover:text-black rounded-3xl">Details</button></Link>
                             </div>
                         </div>
                     </div>
                 </Link>
 
                 <Link to='/sendMoney'>
-                    <div className="card w-50 h-full bg-base-100 shadow-2xl cursor-pointer">
-                        <figure className="px-10 pt-10">
-                            <img src="https://www.bkash.com/uploaded_contents/services/home_images/01-send-money_1675164089657.webp" alt="mobile" className="rounded-xl" />
+                <div style={{boxShadow:'2px 2px 5px'}} className="card w-50 h-full bg-[#FFEAEA] cursor-pointer">
+                        <figure className="px-0 pt-0">
+                           <img className="w-full h-[264px] rounded-md" src={cashout} alt="" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Send money</h2>
-                            <p>Send Money from bKash to any number instantly</p>
+                            <h2 className="card-title text-2xl text-orange-700">Cash Out</h2>
+                            <p>Withdraw money from your Netpay Account</p>
                             <div className="card-actions">
-                                <Link className="text-red-700 font-semibold text-xl" to='/sendMoney'>Learn More</Link>
+                                {/* <button className="btn btn-primary">Learn More</button>
+                                 */}
+                                <Link><button className="btn bg-orange-700 text-white px-6 py-2 hover:bg-orange-600 hover:text-black rounded-3xl">Details</button></Link>
                             </div>
                         </div>
                     </div>
@@ -43,15 +48,17 @@ const FinancialCard = () => {
 
 
                 <Link to='/cashOut'>
-                    <div className="card w-50 h-full bg-base-100 shadow-2xl cursor-pointer">
-                        <figure className="px-10 pt-10">
-                            <img src="https://www.bkash.com/uploaded_contents/services/home_images/03-cashout_1675164155438.webp" alt="mobile" className="rounded-xl" />
+                <div style={{boxShadow:'2px 2px 5px'}} className="card w-50 h-full bg-[#FFEAEA] cursor-pointer">
+                        <figure className="px-0 pt-0">
+                           <img className="w-full h-[264px] rounded-md" src={sendmoney} alt="" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Cash Out</h2>
-                            <p>Withdraw cash anytime from the largest Agent and ATM network</p>
+                            <h2 className="card-title text-2xl text-orange-700">Send Money</h2>
+                            <p>Transfer money from one Netpay account to another</p>
                             <div className="card-actions">
-                                <Link className="text-red-700 font-semibold text-xl" to='/cashOut'>Learn More</Link>
+                                {/* <button className="btn btn-primary">Learn More</button>
+                                 */}
+                                <Link><button className="btn bg-orange-700 text-white px-6 py-2 hover:bg-orange-600 hover:text-black rounded-3xl">Details</button></Link>
                             </div>
                         </div>
                     </div>
@@ -59,15 +66,17 @@ const FinancialCard = () => {
 
 
                 <Link to='/payment'>
-                    <div className="card w-50 h-full bg-base-100 shadow-2xl cursor-pointer">
-                        <figure className="px-10 pt-10">
-                            <img src="https://www.bkash.com/uploaded_contents/services/home_images/04-payment_1675164341242.webp" alt="mobile" className="rounded-xl" />
+                <div style={{boxShadow:'2px 2px 5px'}} className="card w-50 h-full bg-[#FFEAEA] cursor-pointer">
+                        <figure className="px-0 pt-0">
+                           <img className="w-full h-[264px] rounded-md" src={payment} alt="" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Payment</h2>
-                            <p>Making Payment is now much easier through the bKash App</p>
+                            <h2 className="card-title text-2xl text-orange-700">Bill Pay</h2>
+                            <p>Pay your bills through Netpay bill pay service</p>
                             <div className="card-actions">
-                                <Link className="text-red-700 font-semibold text-xl" to='/payment'>Learn More</Link>
+                                {/* <button className="btn btn-primary">Learn More</button>
+                                 */}
+                                <Link><button className="btn bg-orange-700 text-white px-6 py-2 hover:bg-orange-600 hover:text-black rounded-3xl">Details</button></Link>
                             </div>
                         </div>
                     </div>
