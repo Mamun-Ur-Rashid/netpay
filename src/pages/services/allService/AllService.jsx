c88f9fdd8e8dd0dfb0f0e9f7887bfd09ef591aac
nahian255 <nahimn253@gmail.com>
2023-08-14 22:38:31 +0600

    service page half done

--- src/pages/services/allService/AllService.jsx
+++ src/pages/services/allService/AllService.jsx
@@ -1,23 +1,23 @@
 
 const AllService = () => {
     const services = [
-        { serviceName: 'Send Money', serviceIcon: 'QQQQ' },
-        { serviceName: 'Cash Out', serviceIcon: 'QQQQ' },
-        { serviceName: 'Mobile Recharge', serviceIcon: 'QQQQ' },
-        { serviceName: 'Add Money', serviceIcon: 'QQQQ' },
+        { serviceName: 'Send Money', serviceIcon: 'icon' },
+        { serviceName: 'Cash Out', serviceIcon: 'Icon' },
+        { serviceName: 'Mobile Recharge', serviceIcon: 'Icon' },
+        { serviceName: 'Add Money', serviceIcon: 'Icon' },
         { serviceName: 'Add Money', serviceIcon: 'QQQQ' }
     ]
     return (
-        <div>
-            <h1 className="text-center text-4xl py-8 text-red-500"> Slecet a Service</h1>
+        <div className="mb-10">
+            <h1 className="text-center text-4xl py-10 text-red-500"> Slecet a Service</h1>
 
             {/* service item */}
-            <div className="flex md:justify-evenly ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 ">
                 {
                     services?.map((service, index) => (
-                        <div key={index}>
+                        <div key={index} className="bg-white text-black p-4 rounded-lg shadow-md text-center">
                             <p>{service?.serviceIcon}</p>
-                            <h1 className="text-2xl">{service?.serviceName}</h1>
+                            <h1 className="text-2xl mt-6">{service?.serviceName}</h1>
                         </div>
                     ))
                 }
