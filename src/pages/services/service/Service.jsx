d44c7063a90f91eeb2e6d486991df0e32e811977
nahian255 <nahimn253@gmail.com>
2023-08-14 17:06:29 +0600

    service

--- src/pages/services/service/Service.jsx
+++ src/pages/services/service/Service.jsx
@@ -3,9 +3,7 @@ const Service = () => {
     return (
         <div className='mt-16'>
             <h1> this is service page</h1>
-            <h1> create new branch cheacking and need to fix
-
-            </h1>
+            <h1>first setup than work</h1>
         </div>
     );
 };
