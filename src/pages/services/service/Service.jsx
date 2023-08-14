5c7d831e0235df9f66da22ff44ff443b7a34eedd
nahian255 <nahimn253@gmail.com>
2023-08-14 17:04:24 +0600

    new branch

--- src/pages/services/service/Service.jsx
+++ src/pages/services/service/Service.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 
 const Service = () => {
     return (
         <div className='mt-16'>
             <h1> this is service page</h1>
-            <h1> create new branch cheacking</h1>
+            <h1> create new branch cheacking and need to fix
+
+            </h1>
         </div>
     );
 };
