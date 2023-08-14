bce8b80299d7bc2ef953885992f7e2818d045b83
nahian255 <nahimn253@gmail.com>
2023-08-14 14:12:39 +0600

    create home and service page

--- src/layout/router/Router.jsx
+++ src/layout/router/Router.jsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main";
 import Home from "../../pages/Home/home/Home";
-import MobileRecharge from "../../pages/FinancialPlatform/MobileRecharge/MobileRecharge";
-import SendMoney from "../../pages/FinancialPlatform/SendMoney/SendMoney";
-import CashOut from "../../pages/FinancialPlatform/CashOut/CashOut";
-import Payment from "../../pages/FinancialPlatform/Payment/Payment";
 import Service from "../../pages/services/service/Service";
 
 const router = createBrowserRouter([
@@ -20,22 +16,22 @@ const router = createBrowserRouter([
                 path: '/services',
                 element: <Service></Service>
             },
-            {
-                path: '/mobileRecharge',
-                element: <MobileRecharge></MobileRecharge>,
-            },
-            {
-                path: '/sendMoney',
-                element: <SendMoney></SendMoney>
-            },
-            {
-                path: '/cashOut',
-                element: <CashOut></CashOut>
-            },
-            {
-                path: '/payment',
-                element: <Payment></Payment>
-            },
+            // {
+            //     path: '/mobileRecharge',
+            //     element: <MobileRecharge></MobileRecharge>,
+            // },
+            // {
+            //     path: '/sendMoney',
+            //     element: <SendMoney></SendMoney>
+            // },
+            // {
+            //     path: '/cashOut',
+            //     element: <CashOut></CashOut>
+            // },
+            // {
+            //     path: '/payment',
+            //     element: <Payment></Payment>
+            // },
         ]
     }
 ])
--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -1,14 +1,14 @@
 // import Banner from "../../Banner/Banner";
 import Banner from '../../Home/banner/Banner'
-import FinancialCard from "../../FinancialPlatform/FinancialCard/FinancialCard";
 import AccountSystem from '../AccountSystem/AccountSystem';
+import FinancialSection from '../financialSection/FinancialSection';
 
 const Home = () => {
     return (
         <div className="mt-16">
             {/* <Banner></Banner> */}
             <Banner></Banner>
-            <FinancialCard></FinancialCard>
+            <FinancialSection />
             <AccountSystem></AccountSystem>
 
         </div>
