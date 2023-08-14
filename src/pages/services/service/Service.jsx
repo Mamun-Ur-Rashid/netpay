0ec5b115cea0853f44001f3af47b7ef81485fea1
nahian255 <nahimn253@gmail.com>
2023-08-14 17:39:46 +0600

    service start

--- src/pages/services/service/Service.jsx
+++ src/pages/services/service/Service.jsx
@@ -1,9 +1,11 @@
+import AllService from "../allService/AllService";
+import ServiceBanner from "../serviceBanner/ServiceBanner";
 
 const Service = () => {
     return (
         <div className='mt-16'>
-            <h1> this is service page</h1>
-            <h1>first setup than work</h1>
+            <ServiceBanner></ServiceBanner>
+            <AllService></AllService>
         </div>
     );
 };
