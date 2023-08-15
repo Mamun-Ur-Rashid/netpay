27ce8f0c1d275f0f89bd846b6baeaff92144dc2d
muhammadali246397 <muhammadali246397@gmail.com>
2023-08-15 08:46:11 +0600

    complete signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -4,61 +4,73 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
-    const { register, handleSubmit, reset,formState: { errors } } = useForm();
-    const onSubmit = data =>{
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
         console.log(data)
         reset();
-        
+
     };
     console.log(errors);
     return (
         <div style={{ backgroundImage: `url(${background})`, backgroundPosition: 'center', backgroundSize: 'cover', }} className='container mx-auto my-20 py-20'>
-            <div className='text-center fond-bold text-white text-5xl'>Please Login</div>
+            <div className='text-center fond-bold text-white text-5xl'>Please Signup</div>
             <div>
                 <div className="mt-10">
 
                     <div className="hero-content">
-                        <div className="card bg-black bg-opacity-60 md:w-1/3 shadow-2xl">
+                        <div className="card bg-black bg-opacity-60 md:w-3/4 shadow-2xl">
                             <div className="card-body">
                                 <form onSubmit={handleSubmit(onSubmit)}>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text text-base text-white">Email</span>
-                                        </label>
-                                        <input className="input input-bordered" type="email" placeholder="Email" {...register("Email", { required: true, })} />
+                                    <div className='md:flex justify-between'>
+                                        <div className='md:w-[45%]'>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-base text-white">Full Name</span>
+                                                </label>
+                                                <input className="input input-bordered" type="text" placeholder="Full Name..." {...register("name", { required: true, })} />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-base text-white">Password</span>
+                                                </label>
+                                                <input className="input input-bordered" type="password" placeholder="Password..." {...register("password", { required: true, })} />
+                                                
+                                            </div>
+                                        </div>
+                                        <div className='md:w-[45%]'>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-base text-white">Email Address</span>
+                                                </label>
+                                                <input className="input input-bordered" type="email" placeholder="Email Address..." {...register("email", { required: true, })} />
+                                            </div>
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text text-base text-white">Phone Number</span>
+                                                </label>
+                                                <input className="input input-bordered" type="text" placeholder="Phone Number" {...register("number", { required: true, })} />
+                                                
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text text-base text-white">Password</span>
-                                        </label>
-                                        <input className="input input-bordered" type="password" placeholder="Password" {...register("password", { required: true, })} />
-                                        <p className='text-white mt-4'>New to Netpay? <Link to='/signup'><span className='hover:font-semibold hover:text-green-300'>Please Signup</span></Link></p>
+                                                <label className="label">
+                                                    <span className="label-text text-base text-white">Photo</span>
+                                                </label>
+                                                <input type="file" className="file-input file-input-bordered file-input-primary w-full" />
+                                                {/* <input className="input input-bordered" type="password" placeholder="Password" {...register("password", { required: true, })} /> */}
+                                                
+                                            </div>
+                                    <p className='text-white mt-4'>Allready have an account? <Link to='/login'><span className='hover:font-semibold hover:text-green-300'>Please login</span></Link></p>
+                                    <div className='text-center mt-10'>
+                                        <input className="btn bg-orange-700 text-white px-6 py-2 border-none w-full hover:bg-orange-600 hover:text-black rounded-3xl" type="submit" value='Login' />
                                     </div>
-                                   <div className='text-center mt-10'>
-                                   <input className="btn bg-orange-700 text-white px-6 py-2 border-none w-full hover:bg-orange-600 hover:text-black rounded-3xl"type="submit" value='Login' />
-                                   </div>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* <form onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
-                    <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
-                    <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-                    <input type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
-                    <select {...register("Title", { required: true })}>
-                        <option value="Mr">Mr</option>
-                        <option value="Mrs">Mrs</option>
-                        <option value="Miss">Miss</option>
-                        <option value="Dr">Dr</option>
-                    </select>
 
-                    <input {...register("Developer", { required: true })} type="radio" value="Yes" />
-                    <input {...register("Developer", { required: true })} type="radio" value="No" />
-
-                    <input type="submit" />
-                </form> */}
             </div>
         </div >
     );
