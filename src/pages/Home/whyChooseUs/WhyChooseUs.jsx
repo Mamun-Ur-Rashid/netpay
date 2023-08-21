f22630c6904e224618f6d433bb036063570d5854
nahian255 <nahimn253@gmail.com>
2023-08-21 21:25:10 +0600

    why choos us added

---
+++ src/pages/Home/whyChooseUs/WhyChooseUs.jsx
@@ -0,0 +1,16 @@
+import CardSection from "./cardSection/CardSection";
+
+
+const WhyChooseUs = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center text-white font-bold"> Why Choose Us</h1>
+            <h3 className="text-center text-xl mt-4">Help agencies to define their new business objectives and then create professional software.</h3>
+
+            <CardSection></CardSection>
+
+        </div>
+    );
+};
+
+export default WhyChooseUs;
