1dacb0cd5129c36c069fe078d93841ad9eb8d13b
nahian255 <nahimn253@gmail.com>
2023-08-24 16:37:47 +0600

    home page responsive

--- src/pages/Home/TuchPoint/TuchPoint.jsx
+++ src/pages/Home/TuchPoint/TuchPoint.jsx
@@ -2,32 +2,32 @@ import image from '../../../assets/login.png'
 
 const TuchPoint = () => {
     return (
-        <div className='container mx-auto my-20'>
-            <h1 className="text-center font-bold text-5xl my-5"><span className="border-b-4 border-orange-700 text-orange-700">Nearest NetPay Touchpoints</span></h1>
+        <div className='container mx-auto  md:my-12'>
+            <h1 className="text-center font-bold text-4xl md:text-5xl text-red-600">Nearest NetPay Touchpoints</h1>
 
-            <div className='md:flex justify-between mt-20'>
-                <div className='md:w-[30%] px-4'>
-                    <div className='text-center'>
-                        <h4 className='text-3xl font-semibold'>ATM Cash Out Points</h4>
-                        <p className='text-2xl mt-4'>List of Partner Banks' ATM booths to Cash Out</p>
+            <div className='md:flex justify-between mt-6 md:mt-10 bg-[#1B2654] p-8 md:p-14 '>
+                <div className='w-full md:w-[30%] px-4'>
+                    <div className='text-center text-white'>
+                        <h4 className='text-2xl md:text-4xl font-semibold'>ATM Cash Out Points</h4>
+                        <p className='text-sm md:text-xl my-2'>List of Partner Banks&apos; ATM booths to Cash Out</p>
+                    </div>
+                    <div>
+                        <img className='mx-auto' src={image} alt="" />
+                    </div>
+                </div>
+                <div className='md:w-[30%] px-4 py-10 md:py-0'>
+                    <div className='text-center text-white'>
+                        <h4 className='text-2xl md:text-4xl font-semibold'>Customer Care Points</h4>
+                        <p className='text-sm md:text-xl my-2'>List of Netpay Customer Care Centers</p>
                     </div>
                     <div>
                         <img className='mx-auto' src={image} alt="" />
                     </div>
                 </div>
                 <div className='md:w-[30%] px-4'>
-                    <div className='text-center'>
-                        <h4 className='text-3xl font-semibold'>Customer Care Points</h4>
-                        <p className='text-2xl mt-4'>List of Netpay Customer Care Centers</p>
-                    </div>
-                    <div>
-                        <img className='mx-auto' src={image} alt="" />
-                    </div>
-                </div>
-                <div className='md:w-[30%] px-4'>
-                    <div className='text-center'>
-                        <h4 className='text-3xl font-semibold'>Netpay Mercchants</h4>
-                        <p className='text-2xl mt-4'>List of Merchants that accepts Netpay Payment</p>
+                    <div className='text-center text-white'>
+                        <h4 className='text-2xl md:text-4xl font-semibold'>Netpay Mercchants</h4>
+                        <p className='text-sm md:text-xl my-2'>List of Merchants that accepts Netpay Payment</p>
                     </div>
                     <div>
                         <img className='mx-auto' src={image} alt="" />
--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -6,22 +6,24 @@ import FinicialCard from '../FinicialSection/FinicialCard';
 import TuchPoint from '../TuchPoint/TuchPoint';
 import WhyChooseUs from '../whyChooseUs/WhyChooseUs';
 import AnotherSEction from '../whyChooseUs/anotherSection/AnotherSEction';
+import img from "../../../assets/josh-appel.jpg"
 
 const Home = () => {
 
     // Scroll to the top of the page when it loads
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+    // useEffect(() => {
+    //     window.scrollTo(0, 0);
+    // }, []);
 
     return (
         <div className="mt-16 bg-[#070c33]">
             <Banner></Banner>
-            <div className='px-20 md:px-24'>
+            <div className='px-6 md:px-24'>
                 <FinicialCard></FinicialCard>
                 <TuchPoint></TuchPoint>
                 <WhyChooseUs></WhyChooseUs>
                 <AnotherSEction></AnotherSEction>
+
                 <AccountSystem></AccountSystem>
             </div>
 
