6bc345a3ed6c2e99d38c5b6c091491c3a14d5d00
Mamun-Ur-Rashid <engmamun656@gmail.com>
2023-09-14 00:16:58 +0600

    home page poliching and aos apply

--- src/pages/Home/AccountSystem/AccountSystem.jsx
+++ src/pages/Home/AccountSystem/AccountSystem.jsx
@@ -1,10 +1,15 @@
 // import nid from '../../../assets/nid.jpg'
 // import photo from '../../../assets/photo.jpg'
 // import password from '../../../assets/pincode.png'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Title from '../../../shared/title/Title';
 
 
 const AccountSystem = () => {
+
+    Aos.init();
+
     // img 
     const nidImg = 'https://t3.ftcdn.net/jpg/04/39/02/84/360_F_439028436_fuB6JmwKELdcb0OAY6yPW7PwDVtCaWe5.jpg'
 
@@ -14,10 +19,14 @@ const AccountSystem = () => {
 
     return (
         <div className='container mx-auto pb-10 md:pb-14 mt-10 md:mt-14'>
-            <Title title={'Create Netpay Account'} subTitle={'Easy 3 steps to create your Netpay account'}></Title>
+            <div data-aos="zoom-in"
+                data-aos-duration="1000"
+                data-aos-delay="100"> <Title title={'Create Netpay Account'} subTitle={'Easy 3 steps to create your Netpay account'}></Title></div>
             <div className=" mt-2 md:flex justify-between md:gap-x-8 ">
 
-                <div className='bg-[#1B2654] p-8 md:w-1/3 sm:mt-5 px-4'>
+                <div data-aos="zoom-in"
+                    data-aos-duration="1000"
+                    data-aos-delay="100" className='bg-[#4e63b8]  p-8 md:w-1/3 sm:mt-5 px-4 rounded-xl'>
                     <div className=''>
                         <img className='mx-auto w-[200px] h-[164px]' src={nidImg} alt="" />
                     </div>
@@ -26,7 +35,9 @@ const AccountSystem = () => {
                     </div>
                 </div>
 
-                <div className='bg-[#1B2654] p-8 md:w-1/3 sm:mt-5 px-4'>
+                <div data-aos="zoom-in"
+                    data-aos-duration="1000"
+                    data-aos-delay="300" className='bg-[#4e63b8] p-8 md:w-1/3 sm:mt-5 px-4 rounded-xl'>
                     <div className=''>
                         <img className='mx-auto w-[200px] h-[164px]' src={uploadImg} alt="" />
                     </div>
@@ -34,7 +45,9 @@ const AccountSystem = () => {
                         <p className='md:text-xl ml-16 font-bold text-center text-white'>Upload your photo </p>
                     </div>
                 </div>
-                <div className='bg-[#1B2654] p-8 md:w-1/3 sm:mt-5 px-4'>
+                <div data-aos="zoom-in"
+                    data-aos-duration="1000"
+                    data-aos-delay="400" className='bg-[#4e63b8] p-8 md:w-1/3 sm:mt-5 px-4 rounded-xl'>
                     <div className=''>
                         <img className='mx-auto w-[200px] h-[164px]' src={passImg} alt="" />
                     </div>
--- src/pages/Home/TuchPoint/TuchPoint.jsx
+++ src/pages/Home/TuchPoint/TuchPoint.jsx
@@ -1,7 +1,11 @@
 // import image from '../../../assets/login.png'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Title from '../../../shared/title/Title';
 
 const TuchPoint = () => {
+
+    Aos.init();
 
     const atmImg = 'https://img.freepik.com/free-vector/queue-people-standing-using-atm_74855-4812.jpg?w=360'
     const castumerImg = 'https://www.revechat.com/wp-content/uploads/2020/06/Online-customer-service-1280x720.png'
@@ -9,19 +13,29 @@ const TuchPoint = () => {
 
     return (
         <div className='container mx-auto  md:my-12 pt-14'>
+            <div data-aos="zoom-out"
+            data-aos-duration="1000"
+            data-aos-delay="100">
             <Title title={'Our Touchpoints'} subTitle={'Discover Nearest Convenient Locations for Accessing NetPay Services.'}></Title>
-            <div className='md:flex justify-between mt-4 md:mt-10 gap-9 '>
-                <div className='w-full md:w-1/3  px-4 py- md:py-0 bg-[#1B2654] h-96 rounded'>
-                    <div className='text-center p-4 text-white '>
+            </div>
+            <div className='md:flex justify-between mt-4 md:mt-10 gap-9  '>
+                <div 
+                data-aos="zoom-out"
+                data-aos-duration="1000"
+                data-aos-delay="200" className='w-full md:w-1/3  md:py-0  h-96 rounded-xl border border-blue-600'>
+                    <div className='text-center p-4'>
                         <h4 className='text-3xl font-semibold'>ATM Cash Out Points</h4>
                         <p className='text-sm md:text-base py-2'>List of Partner Banks&apos; ATM booths to Cash Out</p>
                     </div>
                     <div >
-                        <img className='mx-auto h-50' src={atmImg} alt="" />
+                        <img className='mx-auto w-fit h-50' src={atmImg} alt="" />
                     </div>
                 </div>
-                <div className='w-full md:w-1/3 px-4 md:py-0 bg-[#1B2654] h-96 rounded'>
-                    <div className='text-center p-4 text-white '>
+                <div
+                data-aos="zoom-out"
+                data-aos-duration="1000"
+                data-aos-delay="300" className='w-full md:w-1/3 px-4 md:py-0  h-96 rounded-xl border border-blue-600'>
+                    <div className='text-center p-4 '>
                         <h4 className='text-3xl font-semibold'>Customer Care Points</h4>
                         <p className='text-sm md:text-base py-2'>List of Netpay Customer Care Centers service</p>
                     </div>
@@ -29,8 +43,11 @@ const TuchPoint = () => {
                         <img className='mx-auto' src={castumerImg} alt="" />
                     </div>
                 </div>
-                <div className='w-full md:w-1/3 px-4 mb-10 md:py-0 bg-[#1B2654] rounded h-[400px] md:h-96'>
-                    <div className='text-center p-4 text-white'>
+                <div 
+                 data-aos="zoom-out"
+                 data-aos-duration="1000"
+                 data-aos-delay="400"className='w-full md:w-1/3 px-4 mb-10 md:py-0 rounded-xl border border-blue-600 h-[400px] md:h-96'>
+                    <div className='text-center p-4 '>
                         <h4 className='text-3xl font-semibold'>Netpay Mercchants</h4>
                         <p className='text-sm md:text-base py-2'>List of Merchants that accepts Netpay Payment</p>
                     </div>
